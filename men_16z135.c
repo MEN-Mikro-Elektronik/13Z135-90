914687a69f5fa85b6f40e2385d414a346e9aa027
Johannes Thumshirn <johannes.thumshirn@men.de>
2014-03-24 08:27:42 +0100

    16z135: Initial Import
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@men.de>

---
+++ men_16z135.c
@@ -0,0 +1,16 @@
+/*
+ * MEN 16z135 High Speed UART
+ *
+ * Copyright (C) 2014 MEN Mikroelektronik GmbH (www.men.de)
+ * Author: Johannes Thumshirn <johannes.thumshirn@men.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2 of the License.
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ":" fmt
+
+#include <linux/module.h>
+
+module_init();
+module_exit();
