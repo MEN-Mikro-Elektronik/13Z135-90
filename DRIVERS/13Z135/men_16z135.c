1a3e25a1dc4263465f7c5b08b381ab20f56ccb7c
KonradJPE <ktomasik@jpembedded.eu>
2019-08-21 08:35:05 +0200

    Add missing license/copyright note
    Issue 'MDIS license clearing'
    https://github.com/MEN-Mikro-Elektronik/13MD05-90/issues/116

--- DRIVERS/13Z135/men_16z135.c
+++ DRIVERS/13Z135/men_16z135.c
@@ -1,13 +1,25 @@
 /*
  * MEN 16z135 High Speed UART
- *
- * Copyright (c) 2014-2019, MEN Mikro Elektronik GmbH
  * Author: Johannes Thumshirn <johannes.thumshirn@men.de>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; version 2 of the License.
- */
+ *---------------------------------------------------------------------------
+ * Copyright 2014-2019, MEN Mikro Elektronik GmbH
+ ****************************************************************************/
+/*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define pr_fmt(fmt) KBUILD_MODNAME ":" fmt
 
 #include <linux/kernel.h>
